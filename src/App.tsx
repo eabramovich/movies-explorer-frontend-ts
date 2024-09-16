40f62c179a01c3c05bd7b151cc3391117ff138a0
Евгения Абрамович <ewg.abramovich2014@yandex.ru>
2024-09-16 13:00:57 +0300

    Add button to UI library

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+       <Button title='Регистрация'/> 
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ui/button/Button.tsx
@@ -0,0 +1,18 @@
+import styles from './Button.module.scss'
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  onClick?: () => void;
+  type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  title,
+  onClick,
+  type = 'button',
+  ...props
+}) => {
+  return (
+    <button className={styles.button} onClick={onClick} type={type} {...props}>{title}</button>
+  )
+}
